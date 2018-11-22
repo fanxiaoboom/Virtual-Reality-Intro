b71cf1154e7e3dc59ca1b157fc48be69556b1aeb
Bran <fanxiaoboom@gmail.com>
2018-11-22 16:35:36 +1100

    original

---
+++ js/my_animate.js
@@ -0,0 +1,79 @@
+
+
+  $(function() {
+  $('.smoothScroll').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 900);
+        return false;
+      }
+    }
+  });
+});
+
+
+
+/*
+ * Noel Delgado | @pixelia_me
+ */
+
+var items = []
+  , point = document.querySelector('svg').createSVGPoint();
+
+function getCoordinates(e, svg) {
+  point.x = e.clientX;
+  point.y = e.clientY;
+  return point.matrixTransform(svg.getScreenCTM().inverse());
+}
+
+function changeColor(e) {
+  document.body.className = e.currentTarget.className;
+}
+
+function Item(config) {
+  Object.keys(config).forEach(function (item) {
+    this[item] = config[item];
+  }, this);
+  this.el.addEventListener('mousemove', this.mouseMoveHandler.bind(this));
+  this.el.addEventListener('touchmove', this.touchMoveHandler.bind(this));
+}
+
+Item.prototype = {
+  update: function update(c) {
+    this.clip.setAttribute('cx', c.x);
+    this.clip.setAttribute('cy', c.y);
+  },
+  mouseMoveHandler: function mouseMoveHandler(e) {
+    this.update(getCoordinates(e, this.svg));
+  },
+  touchMoveHandler: function touchMoveHandler(e) {
+    e.preventDefault();
+    var touch = e.targetTouches[0];
+    if (touch) return this.update(getCoordinates(touch, this.svg));
+  }
+};
+
+[].slice.call(document.querySelectorAll('.item'), 0).forEach(function (item, index) {
+  items.push(new Item({
+    el: item,
+    svg: item.querySelector('svg'),
+    clip: document.querySelector('#clip-'+index+' circle'),
+  }));
+});
+
+[].slice.call(document.querySelectorAll('button'), 0).forEach(function (button) {
+  button.addEventListener('click', changeColor);
+});
+
+
+$('a[data-toggle="tab"]').on('shown.bs.tab', function (e)
+{
+    if(!$("#hambergur_menu_btn").is(":hidden"))
+    {
+        $("#hambergur_menu_btn")[0].click();
+    }
+});
